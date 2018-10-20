6821298e8ec82f36a6efefbfb6636719aa3744b5
Mathieu Blondel <mathieu@mblondel.org>
2018-10-20 09:26:47 +0900

    Move tests.

---
+++ tests/test_fyl_pytorch.py
@@ -0,0 +1,42 @@
+# Author: Mathieu Blondel
+# License: Simplified BSD
+
+import os, sys
+currentdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(currentdir))
+
+import torch
+from torch.autograd import gradcheck, Variable
+
+from fyl_pytorch import SquaredLoss
+from fyl_pytorch import PerceptronLoss
+from fyl_pytorch import LogisticLoss
+from fyl_pytorch import Logistic_OVA_Loss
+from fyl_pytorch import SparsemaxLoss
+
+
+def test_squared_loss():
+    y_true = torch.tensor([[1.7], [2.76], [2.09]], dtype=torch.double)
+    y_pred = torch.tensor([[0.7], [3.76], [1.09]], dtype=torch.double)
+    y_pred = Variable(y_pred, requires_grad=True)
+    loss = SquaredLoss()
+    assert gradcheck(loss, (y_pred, y_true), eps=1e-4, atol=1e-3)
+
+
+def test_classification_losses():
+    for loss in (PerceptronLoss(), LogisticLoss(), Logistic_OVA_Loss(), SparsemaxLoss()):
+        y_true = torch.tensor([0, 0, 1, 2], dtype=torch.long)
+        theta = torch.tensor(torch.randn(y_true.shape[0], 3), dtype=torch.double)
+        theta = Variable(theta, requires_grad=True)
+        assert gradcheck(loss, (theta, y_true), eps=1e-4, atol=1e-3)
+
+
+def test_classification_losses_multilabel():
+    for loss in (LogisticLoss(), Logistic_OVA_Loss(), SparsemaxLoss()):
+        y_true = torch.tensor([[1, 0, 0],
+                               [1, 0, 0],
+                               [0, 1, 0],
+                               [0, 0, 1]], dtype=torch.double)
+        theta = torch.tensor(torch.randn(*y_true.shape), dtype=torch.double)
+        theta = Variable(theta, requires_grad=True)
+        assert gradcheck(loss, (theta, y_true), eps=1e-4, atol=1e-3)
---
+++ tests/test_fyl_tensorflow.py
@@ -0,0 +1,48 @@
+# Author: Mathieu Blondel
+# License: Simplified BSD
+
+import os, sys
+currentdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(currentdir))
+
+import tensorflow as tf
+
+from fyl_tensorflow import squared_loss
+from fyl_tensorflow import logistic_loss
+from fyl_tensorflow import logistic_ova_loss
+from fyl_tensorflow import sparsemax_loss
+
+
+def test_squared_loss():
+    with tf.Session() as sess:
+        y_true = tf.constant([[1.7], [2.76], [2.09]])
+        y_pred = tf.constant([[0.7], [3.76], [1.09]])
+        loss = squared_loss(y_true, y_pred)
+        error = tf.test.compute_gradient_error(y_pred, y_pred.shape,
+                                               loss, loss.shape)
+        assert error < 1e-3
+
+
+def test_classification_losses():
+    for loss_func in (logistic_loss, logistic_ova_loss, sparsemax_loss):
+        with tf.Session() as sess:
+            y_true = tf.constant([0, 0, 1, 2])
+            theta = tf.random_normal((4, 3))
+            loss = loss_func(y_true, theta)
+            error = tf.test.compute_gradient_error(theta, theta.shape,
+                                                   loss, loss.shape)
+            assert error < 1e-3
+
+
+def test_classification_losses_multilabel():
+    for loss_func in (logistic_loss, logistic_ova_loss, sparsemax_loss):
+        with tf.Session() as sess:
+            y_true = tf.constant([[1, 0, 0],
+                                   [1, 0, 0],
+                                   [0, 1, 0],
+                                   [0, 0, 1]])
+            theta = tf.random_normal((4, 3))
+            loss = loss_func(y_true, theta)
+            error = tf.test.compute_gradient_error(theta, theta.shape,
+                                                   loss, loss.shape)
+            assert error < 1e-3
