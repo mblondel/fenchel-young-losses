b2a48487c22ae9dc2b6690587dd5a5dfdf5b4851
okba bekhelifi <okba.bekhelifi@gmail.com>
2022-10-21 21:43:00 +0200

    - Updated Tensorflow tests On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   tests/test_fyl_tensorflow.py

--- tests/test_fyl_tensorflow.py
+++ tests/test_fyl_tensorflow.py
@@ -1,4 +1,5 @@
 # Author: Mathieu Blondel
+#         Okba Bekhelifi
 # License: Simplified BSD
 
 import os, sys
@@ -14,35 +15,35 @@ from fyl_tensorflow import sparsemax_loss
 
 
 def test_squared_loss():
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         y_true = tf.constant([[1.7], [2.76], [2.09]])
         y_pred = tf.constant([[0.7], [3.76], [1.09]])
         loss = squared_loss(y_true, y_pred)
-        error = tf.test.compute_gradient_error(y_pred, y_pred.shape,
+        error = tf.compat.v1.test.compute_gradient_error(y_pred, y_pred.shape,
                                                loss, loss.shape)
         assert error < 1e-3
 
 
 def test_classification_losses():
     for loss_func in (logistic_loss, logistic_ova_loss, sparsemax_loss):
-        with tf.Session() as sess:
+        with tf.compat.v1.Session() as sess:
             y_true = tf.constant([0, 0, 1, 2])
-            theta = tf.random_normal((4, 3))
+            theta = tf.random.normal((4, 3))
             loss = loss_func(y_true, theta)
-            error = tf.test.compute_gradient_error(theta, theta.shape,
+            error = tf.compat.v1.test.compute_gradient_error(theta, theta.shape,
                                                    loss, loss.shape)
             assert error < 1e-3
 
 
 def test_classification_losses_multilabel():
     for loss_func in (logistic_loss, logistic_ova_loss, sparsemax_loss):
-        with tf.Session() as sess:
+        with tf.compat.v1.Session() as sess:
             y_true = tf.constant([[1, 0, 0],
                                    [1, 0, 0],
                                    [0, 1, 0],
                                    [0, 0, 1]])
-            theta = tf.random_normal((4, 3))
+            theta = tf.random.normal((4, 3))
             loss = loss_func(y_true, theta)
-            error = tf.test.compute_gradient_error(theta, theta.shape,
+            error = tf.compat.v1.test.compute_gradient_error(theta, theta.shape,
                                                    loss, loss.shape)
             assert error < 1e-3
